0005e32566920e39fc2a151503b55ecce904e0af
Nishant Nepal <nishant.nepal@newsignature.com>
2020-12-04 15:19:38 -0500

    Added Docker Push

--- dockerBuildPush.ps1
+++ dockerBuildPush.ps1
@@ -3,3 +3,7 @@ cd C:\Projects\githubRepos\DockerComposeDemo
 docker build -t nishantnepal/demosvc-a:v1 -f ./WepAppA/DockerFile .
 docker build -t nishantnepal/demosvc-b:v1 -f ./WebAppB/DockerFile .
 docker build -t nishantnepal/demosvc-c:v1 -f ./WebAppC/DockerFile .
+
+docker push nishantnepal/demosvc-a:v1
+docker push nishantnepal/demosvc-b:v1
+docker push nishantnepal/demosvc-c:v1
